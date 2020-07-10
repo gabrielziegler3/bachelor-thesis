f9abe3d5133ea3e2fe1f88aaeb426b9bb285c5cb
Gabriel Ziegler <gabrielziegler3@gmail.com>
2020-07-09 22:36:32 -0300

    Add thesis template, compilation files and introduction
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

---
+++ compila.bat
@@ -0,0 +1,13 @@
+pdflatex fga_template
+bibtex fga_template
+pdflatex fga_template
+pdflatex fga_template
+
+rm "fga_template.aux"
+rm "fga_template.bbl"
+rm "fga_template.blg"
+rm "fga_template.lof"
+rm "fga_template.log"
+rm "fga_template.lot"
+rm "fga_template.out"
+rm "fga_template.toc"
