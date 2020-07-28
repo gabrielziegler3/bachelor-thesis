6adfaf439b59bc970aba696341b61c82a6d10394
Gabriel Ziegler <gabrielziegler3@gmail.com>
2020-07-28 11:28:22 -0300

    Rename files to gabrielziegler_2020
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- compila.bat
+++ compila.bat
@@ -1,13 +1,13 @@
-pdflatex fga_template
-bibtex fga_template
-pdflatex fga_template
-pdflatex fga_template
+pdflatex gabrielziegler_2020
+bibtex gabrielziegler_2020
+pdflatex gabrielziegler_2020
+pdflatex gabrielziegler_2020
 
-rm "fga_template.aux"
-rm "fga_template.bbl"
-rm "fga_template.blg"
-rm "fga_template.lof"
-rm "fga_template.log"
-rm "fga_template.lot"
-rm "fga_template.out"
-rm "fga_template.toc"
+rm "gabrielziegler_2020.aux"
+rm "gabrielziegler_2020.bbl"
+rm "gabrielziegler_2020.blg"
+rm "gabrielziegler_2020.lof"
+rm "gabrielziegler_2020.log"
+rm "gabrielziegler_2020.lot"
+rm "gabrielziegler_2020.out"
+rm "gabrielziegler_2020.toc"
